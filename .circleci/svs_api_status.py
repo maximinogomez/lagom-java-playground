7f1238d0654e750c2bc8f163960f41884321f41d
Maximino Gomez <maximino.gomez@gmail.com>
2018-06-18 16:18:21 -0700

    more changes

--- .circleci/svs_api_status.py
+++ .circleci/svs_api_status.py
@@ -8,9 +8,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 
-ENV_TAG_NAMES = {'ENV-PLAT', 'ENV-DEV'}
-
-API_STATUS_PATH = {
+API_STATUS_PATHS = {
     'users': '/api/users/status',
     'auth': '/api/auth/status',
     'conversations': '/api/conversations/status',
@@ -43,25 +41,10 @@ def get_api_status(endpoint):
     return api_response_values
 
 
-def get_env_host_url(env_tag):
-    environments = {
-        'plat': 'https://pivotus.plat.engage.pivotus.io',
-        # 'dev': 'https://pivotus.dev.engage.pivotus.io',
-        # 'qa': 'https://pivotus.dev.engage.pivotus.io'
-    }
+def start_api_checks(host_url, expected_build_number):
+    print('*** Start API status checks deployed at [ {0} ] ***'.format(host_url))
 
-    if env_tag.lower().startswith('env-'):
-        env = env_tag[len('env-'):]
-        return environments[env.lower()] if env.lower() in environments else ''
-    else:
-        return ''
-
-
-def start(host_url, expected_build_number):
-    time.sleep(10)
-
-    for k, v in API_STATUS_PATH.items():
-        print("----------")
+    for k, v in API_STATUS_PATHS.items():
         api_response_values = get_api_status(host_url + v)
 
         if 'buildNumber' in api_response_values:
@@ -75,20 +58,67 @@ def start(host_url, expected_build_number):
                 print('Expected build number [{0}] DID matched actual build number [{1}]'
                       .format(expected_build_number, actual_build_number))
             else:
-                sys.exit("Expected build number [{0}] DID NOT matched actual build number [{1}]"
+                sys.exit('Expected build number [{0}] DID NOT matched actual build number [{1}]'
                          .format(expected_build_number, actual_build_number))
+        else:
+            print('Response did not have a build number for Endpoint: {0} \nStatus: {1}'
+                  .format(host_url + v, api_response_values['code']))
+    print('*** End API status checks ***')
+
+
+def trigger_ci_engage_deploy_job(env_name):
+    target_urls = {
+        'plat': 'https://jenkins.pivotusventures.com/jenkins/job/Engage-Deploy-Plat/build'
+    }
+
+    jenkins_username = 'maximinogomez'  # Change me!
+    jenkins_password = 'c30e09bc41d4b026d4fee885f95d9a6b'  # Change me!
+
+    has_errors = False
+    if env_name in target_urls and target_urls[env_name] == 'plat':
+        try:
+            target_url = target_urls[env_name]
+            response = requests_retry_session().post(target_url, auth=(jenkins_username, jenkins_password))
+
+            if response.status_code == 201:
+                print('Remote jenkins job response status: {0}'.format(response.status_code))
+                has_errors = False
+            else:
+                print('Remote jenkins job response status: {0}'.format(response.status_code))
+                has_errors = True
+        except Exception as x:
+            print('Request failed!')
+            has_errors = True
+    return has_errors
+
+
+def get_env_host_url(env_name):
+    environments = {
+        'plat': 'https://pivotus.plat.engage.pivotus.io',
+        # 'dev': 'https://pivotus.dev.engage.pivotus.io'
+    }
+    return environments[env_name] if env_name in environments else ''
+
+
+def to_env_name(env_tag):
+    return (env_tag[len('env-'):]).lower() if env_tag.lower().startswith('env-') else ''
 
 
 def main():
-    if 'CIRCLE_TAG' in os.environ and os.environ['CIRCLE_TAG'] in ENV_TAG_NAMES:
-        in_host_url = get_env_host_url(os.environ['CIRCLE_TAG'])
-        in_expected_build_number = os.environ['CIRCLE_SHA1'][0:10]
+    env_tag_names = {'ENV-PLAT'}
 
-        print('*** Start API status checks deployed at [ {0} ] ***'.format(in_host_url))
-        start(in_host_url, in_expected_build_number)
-        print('*** End API status checks ***')
-    else:
-        print("No match for env tag!")
+    if 'CIRCLE_TAG' in os.environ and os.environ['CIRCLE_TAG'] in env_tag_names:
+        env_name = to_env_name(os.environ['CIRCLE_TAG'])
+        expected_build_number = os.environ['CIRCLE_SHA1'][0:10]
+
+        env_host_url = get_env_host_url(env_name)
+
+        if env_host_url == '':
+            print('No host url found for env name: {0}'.format(env_name))
+        else:
+            if trigger_ci_engage_deploy_job(env_name):
+                time.sleep(10)
+                start_api_checks(env_host_url, expected_build_number)
 
 
 if __name__ == '__main__':
